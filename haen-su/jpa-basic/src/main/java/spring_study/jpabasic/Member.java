d555102a69e9dba545f5b552d021e06620d2c166
haen-su <haensuya@gmail.com>
2023-01-06 23:41:14 +0900

    0106 연관관계 매핑

--- haen-su/jpa-basic/src/main/java/spring_study/jpabasic/Member.java
+++ haen-su/jpa-basic/src/main/java/spring_study/jpabasic/Member.java
@@ -1,14 +1,19 @@
 package spring_study.jpabasic;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Member {
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    private String username;
 
     public Long getId() {
         return id;
@@ -18,12 +23,19 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public Team getTeam() {
+        return team;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setTeam(Team team) {
+        this.team = team;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
